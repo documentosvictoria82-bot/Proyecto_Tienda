bea79e6b3b4b36cce563f6796e36fe6836eacbbd
Victoria <documentosvictoria82@gmail.com>
2026-03-12 21:06:23 -0500

    Se activan los puertos nuevamente, porque vercel no me permite verlos.

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const usuarioRuta = require('./src/rutas/usuariosRutas')
 server.use(cors());
 
 
-//const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 conectar()
 
 server.use(express.json());
@@ -33,7 +33,7 @@ res.send('API TIENDA')
     
 server.use('/api', ruta)
 server.use('/api', usuarioRuta)
-// server.listen(PORT, ()=>{
-//     console.log(`servidor corriendo en http://localhost:${PORT}`);
-// })
+ server.listen(PORT, ()=>{
+     console.log(`servidor corriendo en http://localhost:${PORT}`);
+ })
 module.exports = server;
