6036596ce0db3ca19a861701f5bdadd50eece9dd
Victoria <documentosvictoria82@gmail.com>
2026-03-17 12:07:18 -0500

    se quita el /. de windows location en el archivo de login

--- Frontend/js/login.js
+++ Frontend/js/login.js
@@ -33,7 +33,7 @@ if (response.ok) {
     if (data.role === 'admin') {
         window.location.href = "../pages/creacionProducto.html";
     } else {
-        window.location.href = "./index.html";
+        window.location.href = "index.html";
     }
 } else {
     alert(data.error);
