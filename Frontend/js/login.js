887ad69e153c89dde2465c925d00c4a664ddb577
Alejandra <mariallanten97@gmail.com>
2026-03-18 15:42:43 -0500

    se deja login.js como estaba

--- Frontend/js/login.js
+++ Frontend/js/login.js
@@ -1,45 +1,42 @@
+//const url = new URLSearchParams(window.location.search)
+//console.log(url.getAll('idproducto'));
 const API = "https://proyecto-tienda-rho.vercel.app/api/login"
 
 const form = document.getElementById("loginForm")
 
 form.addEventListener("submit", async (e)=>{
 
-    e.preventDefault()
+e.preventDefault()
 
-    const email = document.getElementById("email").value
-    const password = document.getElementById("password").value
+const email = document.getElementById("email").value
+const password = document.getElementById("password").value
 
-    const response = await fetch(API,{
-        method:"POST",
-        headers:{
-            "Content-Type":"application/json"
-        },
-        body: JSON.stringify({email,password})
-    })
+const response = await fetch(API,{
+method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
 
-    const data = await response.json()
+body: JSON.stringify({email,password})
+
+})
+
+const data = await response.json()
 
 if (response.ok) {
+    localStorage.setItem("token", data.token);
+    localStorage.setItem("role", data.role); 
 
-    localStorage.setItem("token", data.token)
-    localStorage.setItem("role", data.role)
+    alert("Login correcto");
 
-    // 🔥 ESTA ES LA LÍNEA CLAVE
-    localStorage.setItem("usuario", JSON.stringify({
-        email: email,
-        role: data.role
-    }))
-
-    alert("Login correcto")
-
+    // Redirección basada en rol
     if (data.role === 'admin') {
-        window.location.href = "../pages/creacionProducto.html"
+        window.location.href = "../pages/creacionProducto.html";
     } else {
-        window.location.href = "/index.html"
+        window.location.href = "/index.html";
     }
-
 } else {
-        alert(data.error)
-    }
+    alert(data.error);
+}
 
 })
